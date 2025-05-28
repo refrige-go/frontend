6fb0a96015002956de094a7ad9d695b9654ec0ee
전우선 <jeonwooseon@naver.com>
2025-05-28 16:09:36 +0900

    컴포넌트 파일 분리, 로그인 동적으로 변경

--- src/hooks/useIngredients.js
+++ src/hooks/useIngredients.js
@@ -1,15 +1,15 @@
 import { useEffect, useState } from 'react';
 import api from '../lib/api';
 
-export function useIngredients() {
+export function useIngredients(userId) {
   const [ingredients, setIngredients] = useState([]);
 
-  // 재료 불러오기 함수 밖으로 분리
   async function fetchIngredients() {
+    if (!userId) return; // userId 없으면 요청 안 함
     try {
       const res = await api.get('/user-ingredients', {
         params: {
-          userId: '1',
+          userId: userId,
         },
       });
       setIngredients(res.data);
@@ -18,11 +18,9 @@ export function useIngredients() {
     }
   }
 
-  // 재료 삭제 함수 추가
   async function deleteIngredient(id) {
     try {
       await api.delete(`/user-ingredients/${id}`);
-      // 삭제 성공하면 재료 다시 불러오기
       await fetchIngredients();
       return true;
     } catch (error) {
@@ -33,7 +31,7 @@ export function useIngredients() {
 
   useEffect(() => {
     fetchIngredients();
-  }, []);
+  }, [userId]);
 
   return {
     ingredients,
